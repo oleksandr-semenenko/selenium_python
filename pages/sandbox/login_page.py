f276eb98e5ba1f90dcfc2c57b09dcb764de3fadf
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-06-06 11:37:08 +0200

    add a Login Page Object

---
+++ pages/sandbox/login_page.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class LoginPage:
+    __url = "https://semenenko.sandbox.first.institute/web/login"
+    __username_field = (By.ID, "login")
+    __password_field = (By.ID, "password")
+    __login_button = (By.XPATH, "//button[@class='btn btn-primary']")
+
+    def __inint__(self, driver: WebDriver):
+        self._driver = driver
+
+    def open(self):
+        self._driver.get(self.__url)
+
+    def execute_login(self, username: str, password: str):
+        self._driver.find_element(self.__username_field).send_keys(username)
+
+        self._driver.find_element(self.__password_field).send_keys(password)
+
+        self._driver.find_element(self.__login_button).click()
+        time.sleep(5)
