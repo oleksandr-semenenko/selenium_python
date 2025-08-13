766a0c256248e6eb13fb3ec86b81c8ae7d3b10c3
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-08-13 15:49:59 +0200

    changes employee page

--- pages/sandbox/employees_page.py
+++ pages/sandbox/employees_page.py
@@ -1,12 +1,9 @@
 import os
 from dataclasses import dataclass
-from typing import Callable, Union, Literal
 
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.expected_conditions import WebDriverOrWebElement
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
