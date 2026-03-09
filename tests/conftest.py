76c330910c3c3fc3d99372b1a43fbf1bfada871b
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2026-03-09 17:11:46 +0100

    Add reports fixtures parameterized tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,41 @@
 import os
 
 import pytest
+from selenium import webdriver
 
 from pages.sandbox.login_page import LoginPage
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Choose browser to execute tests: chrome or firefox",
+    )
+
+# @pytest.fixture(params=["chrome", "firefox"])
+@pytest.fixture()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    # browser = request.param
+    print(f"Creating selenium driver for {browser}")
+
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox' but got {browser}")
+
+    #driver.maximize_window()
+
+    yield driver
+    print(f"Closing selenium driver for {browser}")
+    driver.quit()
+
+    driver.quit()
 
 
 @pytest.fixture()
